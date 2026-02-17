a0385bc2da54807886ee3218bc18d0b80275b4ed
bidisha-2912 <sbidisha974@gmail.com>
2026-02-17 12:35:14 +0530

    fibonacci2.py

---
+++ fibonacci2.py
@@ -0,0 +1,9 @@
+#FIBONACCI SERIES USING RECURSION
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+a=int(input("Enter the number of terms:"))
+for i in range(a):
+    print(fibonacci(i),end=" ")
